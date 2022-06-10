3e6caa32b3cc21c6af5af0696a338acaaa7fa6bc
discodevl <donato.cutro3@gmail.com>
2022-06-10 19:42:00 +0200

    styles

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -8,17 +8,8 @@ function ModelViewer() {
 
   console.log(hideSideBar)
 
-  useEffect(() => {
-    if(hideSideBar) {
-      // document.querySelector('model-viewer').style.width = '95vw'
-    } else {
-      // document.querySelector('model-viewer').style.width = '75vw'
-    }
-
-  },[hideSideBar]);
-
   return (
-    <div className={styles.container}>
+    <div className={hideSideBar ? styles.containerLarge : styles.container}>
       <model-viewer
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
         src="https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb" camera-controls
