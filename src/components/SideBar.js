6a04d3ebedc84cf54d6cc94b0b4eb08f2b43d077
discodevl <donato.cutro3@gmail.com>
2022-07-09 22:58:19 +0200

    side color

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { configActions } from "../redux/config/config";
 import { useDispatch, useSelector } from "react-redux";
-import maximize from "../assets/maximize-2.svg";
-import minimize from "../assets/minimize-2.svg";
 import styles from "./SideBar.module.css";
 import Luminosity from "../actions/Luminosity";
 import Background from "../actions/Background";
@@ -22,7 +20,7 @@ function SideBar(props) {
   return (
     <div className={styles.container} style={props.hide ? {display: "none"} : {display: "block"}}>
       {/* {!hide ? <img src={minimize} onClick={hideHandler} /> : <img src={maximize} onClick={hideHandler} />} */}
-      <div className={styles.componentsWrapper} style={hide ? {display: "none"} : {display: ""}}>
+      <div className={styles.componentsWrapper} >
         {props.tab === 1 && <Luminosity />}
         {props.tab === 2 && <TextureSelector />}
         {props.tab === 3 && <Background />}
