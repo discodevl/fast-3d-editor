6374094410183c68d1b95f1a5a35b09981fee345
discodevl <donato.cutro3@gmail.com>
2022-10-27 02:41:38 +0200

    paypal donation button

--- src/actions/Doc.js
+++ src/actions/Doc.js
@@ -1,9 +1,13 @@
+import { DonateApp } from '../util/PayPal';
 import styles from './Doc.module.css';
+
 
 function Doc() {
   return (
     <div className={styles.container}>
         <h3>Welcome to fast 3d editor :)</h3>
+        <span>Enjoying editing 3d models? Consider to make a small donation!</span>
+        <DonateApp />
     </div>
   )
 }
---
+++ src/util/PayPal.js
@@ -0,0 +1,105 @@
+import { useEffect, useState, useRef } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+// There are 3 ways to keep the amount from being stale in our createOrder callback:
+// 1. Use amountRef.current to keep track of amount changes
+// 2. Use the forceReRender prop (which causes flashing)
+// 3. Use the key property (which also causes flashing)
+
+const DonateButton = ({ currency, amount }) => {
+  
+    const amountRef = useRef(amount);
+  
+  useEffect(() => {
+    amountRef.current = amount;
+  }, [amount]);
+
+  return (
+    <PayPalButtons
+    //   forceReRender={[currency, amount]}
+      style={{ color: "black", label: "donate" }}
+      fundingSource="paypal"
+      createOrder={(data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              amount: {
+                value: amountRef.current,
+                breakdown: {
+                  item_total: {
+                    currency_code: currency,
+                    value: amountRef.current
+                  }
+                }
+              },
+              items: [
+                {
+                  name: "Fast 3d Editor",
+                  description:
+                    "Help the 'fast 3d editor' project with a small amount.",
+                  quantity: "1",
+                  unit_amount: {
+                    currency_code: currency,
+                    value: amountRef.current
+                  },
+                  category: "DONATION"
+                }
+              ]
+            }
+          ]
+        });
+      }}
+    />
+  );
+};
+
+function DonateForm() {
+  const [amount, setAmount] = useState("3.00");
+  return (
+    <form className="DonateForm">
+      <AmountPicker
+        onAmountChange={(e) => {
+          setAmount(e.target.value);
+        }}
+      />
+      <DonateButton currency="EUR" amount={amount} />
+    </form>
+  );
+}
+
+function AmountPicker({ onAmountChange }) {
+  return (
+    <fieldset onChange={onAmountChange}>
+      <legend>Donation Amount</legend>
+      <label>
+        <input type="radio" value="3.00" defaultChecked="true" name="amount" />
+        3.00
+      </label>
+      <label>
+        <input type="radio" value="5.00" name="amount" id="radio-6" />
+        5.00
+      </label>
+      <label>
+        <input type="radio" value="10.00" name="amount" id="radio-9" />
+        10.00
+      </label>
+    </fieldset>
+  );
+}
+
+console.log()
+
+export function DonateApp() {
+  return (
+    <PayPalScriptProvider
+      options={{
+        "client-id": process.env.REACT_APP_PP_CLIENT_ID,
+        components: "buttons",
+        currency: "EUR"
+      }}
+    >
+      {/*Infos and img */}
+      <DonateForm />
+    </PayPalScriptProvider>
+  );
+}
