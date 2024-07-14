d8ebd9699908916a66539c5b7df6cd77bbf41119
discodevl <dcutro@disroot.org>
2024-07-14 03:14:38 +0200

    fixing styles pt1

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {modelActions} from '../redux/model/model';
+import { modelActions } from "../redux/model/model";
 import glb from "../assets/Astronaut.glb";
 import styles from "./ModelViewer.module.css";
+import { Tooltip } from "react-tooltip";
 
-function ModelViewer({isModelLoaded}) {
+function ModelViewer({ isModelLoaded }) {
   const inputRef = useRef();
   const dispatch = useDispatch();
 
@@ -15,49 +16,45 @@ function ModelViewer({isModelLoaded}) {
 
   const bgColor = useSelector((state) => state.config.background_color);
 
+  // function handleBtnUpload(e) {
+  //   e.preventDefault();
+  //   inputRef.current.click();
+  // }
+
+  function handleFile(e) {
+    const modelUpload = e.target.files[0];
+    const src = URL.createObjectURL(modelUpload);
+    dispatch(modelActions.setSrc(src));
+  }
+
+  useEffect(() => {
+    dispatch(modelActions.setSrc(glb));
+  }, []);
+
   function handleBtnUpload(e) {
     e.preventDefault();
     inputRef.current.click();
   }
 
-  function handleFile(e) {
-    const modelUpload = e.target.files[0];
-    const src = URL.createObjectURL(modelUpload)
-    dispatch(modelActions.setSrc(src));
-  }
-
-  function setDemoGlb() {
-    dispatch(modelActions.setSrc(glb));
-  }
-
-  useEffect(() => {
-    if(src) {
-      isModelLoaded(true);
-    } else{
-      isModelLoaded(false);
-    }
-  }, [src]);
-
   return (
-    <div className={styles.container} style={{backgroundColor: bgColor}}>
-      {!src ? (
-        <div className={styles.subContainer}>
-          <input
-            ref={inputRef}
-            type="file"
-            accept=".glb"
-            onChange={handleFile}
-            style={{ display: "none" }}
-          />
-          <button className={styles.btn} onClick={handleBtnUpload}>
-            Upload
-          </button>
-          <p className={styles.paragraph}>or</p>
-          <button className={styles.btn} onClick={setDemoGlb}>
-            start
-          </button>
-        </div>
-      ) : (
+    <>
+      <input
+        ref={inputRef}
+        type="file"
+        accept=".glb"
+        onChange={handleFile}
+        style={{ display: "none" }}
+      />
+      <button
+        className={styles.options}
+        data-tooltip-id="glb_tooltip"
+        data-tooltip-content="Upload you own .glb"
+        onClick={handleBtnUpload}
+      >
+        upd_ico
+      </button>
+      <Tooltip id="glb_tooltip" />
+      <div className={styles.container} style={{ backgroundColor: bgColor }}>
         <model-viewer
           id="mv"
           alt="model viewer with fast 3d editor"
@@ -67,8 +64,8 @@ function ModelViewer({isModelLoaded}) {
           shadow-intensity={shadowIntensity}
           shadow-softness={shadowSoftness}
         ></model-viewer>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
+import 'react-tooltip/dist/react-tooltip.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
